93a1571cd7867a7b1fde86176d33b147c066a746
Anton Dion <ed.and.ein@proton.me>
2026-01-21 10:45:49 +0100

    Initial commit: Architecture Monorepo Spring Boot + Vue.js

---
+++ backend/src/main/java/fr/insa/gaming_chair_list/GamingChairListApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.gaming_chair_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingChairListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingChairListApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/insa/gaming_chair_list/GamingChairListApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.gaming_chair_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingChairListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
