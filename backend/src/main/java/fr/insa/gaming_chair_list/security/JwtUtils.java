1254414ad1f5acd25ee3b43728570d7ed56b4ed3
Anton Dion <ed.and.ein@proton.me>
2026-01-23 10:23:54 +0100

    Ajout de JWTS

---
+++ backend/src/main/java/fr/insa/gaming_chair_list/security/JwtUtils.java
@@ -0,0 +1,39 @@
+package fr.insa.gaming_chair_list.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    // Injection de la valeur depuis application.properties
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    // chiffrement
+    private Key getSignKey() {
+        // Décode la cle
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // voir doc JWts builder
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
